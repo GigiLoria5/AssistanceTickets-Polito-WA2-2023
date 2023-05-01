fd7dd01ca74b92119cc9a7a16c67a13d7857045e
Pepaunz <peppe.dojo@gmail.com>
2023-05-01 16:13:58 +0200

    feat: add ChatIsInactiveException

--- server/src/main/kotlin/it/polito/wa2/g29/server/advice/ApplicationExceptionHandler.kt
+++ server/src/main/kotlin/it/polito/wa2/g29/server/advice/ApplicationExceptionHandler.kt
@@ -1,10 +1,6 @@
 package it.polito.wa2.g29.server.advice
 
-import it.polito.wa2.g29.server.exception.DuplicateProfileException
-import it.polito.wa2.g29.server.exception.ExpertNotFoundException
-import it.polito.wa2.g29.server.exception.ProductNotFoundException
-import it.polito.wa2.g29.server.exception.ProfileNotFoundException
-import it.polito.wa2.g29.server.exception.TicketNotFoundException
+import it.polito.wa2.g29.server.exception.*
 import it.polito.wa2.g29.server.utils.ErrorMessage
 import jakarta.validation.ConstraintViolationException
 import org.springframework.core.Ordered
@@ -45,6 +41,13 @@ class ApplicationExceptionHandler {
     @ExceptionHandler(value = [ConstraintViolationException::class, MethodArgumentNotValidException::class, HttpMessageNotReadableException::class, MethodArgumentTypeMismatchException::class])
     fun handleValidationFailedException(exception: Exception): ResponseEntity<ErrorMessage> {
         val errorMessage = ErrorMessage("validation of request failed")
+        return ResponseEntity(errorMessage, HttpStatus.UNPROCESSABLE_ENTITY)
+    }
+
+    // 422 - Chat Error message
+    @ExceptionHandler(value = [ChatIsInactiveException::class])
+    fun handleValidationFailedExceptionWithErrorMessage(exception: Exception): ResponseEntity<ErrorMessage> {
+        val errorMessage = ErrorMessage(exception.message.orEmpty())
         return ResponseEntity(errorMessage, HttpStatus.UNPROCESSABLE_ENTITY)
     }
 
