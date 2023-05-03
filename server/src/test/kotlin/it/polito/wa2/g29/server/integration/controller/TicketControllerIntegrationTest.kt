b8de25c5a126112b5b37e71e90b302249029d749
Andred00 <s303339@studenti.polito.it>
2023-05-03 10:20:30 +0200

    feat: first part of integration test for TicketController

--- server/src/test/kotlin/it/polito/wa2/g29/server/integration/controller/TicketControllerIntegrationTest.kt
+++ server/src/test/kotlin/it/polito/wa2/g29/server/integration/controller/TicketControllerIntegrationTest.kt
@@ -1,13 +1,20 @@
 package it.polito.wa2.g29.server.integration.controller
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import it.polito.wa2.g29.server.dto.toDTO
 import it.polito.wa2.g29.server.integration.AbstractTestcontainersTest
+import it.polito.wa2.g29.server.model.Expert
+import it.polito.wa2.g29.server.model.Product
+import it.polito.wa2.g29.server.model.Profile
 import it.polito.wa2.g29.server.model.Ticket
+import it.polito.wa2.g29.server.repository.ExpertRepository
 import it.polito.wa2.g29.server.repository.ProductRepository
 import it.polito.wa2.g29.server.repository.ProfileRepository
 import it.polito.wa2.g29.server.repository.TicketRepository
 import it.polito.wa2.g29.server.utils.TestProductUtils
 import it.polito.wa2.g29.server.utils.TestProfileUtils
 import it.polito.wa2.g29.server.utils.TestTicketUtils
+import org.hamcrest.Matchers.*
 import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
@@ -34,9 +41,10 @@ class TicketControllerIntegrationTest : AbstractTestcontainersTest() {
     lateinit var testTickets: List<Ticket>
 
     @BeforeAll
-    fun prepare(@Autowired profileRepository: ProfileRepository, @Autowired productRepository: ProductRepository) {
+    fun prepare(@Autowired profileRepository: ProfileRepository, @Autowired productRepository: ProductRepository, @Autowired expertRepository: ExpertRepository) {
         productRepository.deleteAllInBatch()
         profileRepository.deleteAllInBatch()
+        expertRepository.deleteAllInBatch()
         TestTicketUtils.products = TestProductUtils.insertProducts(productRepository)
         TestTicketUtils.profiles = TestProfileUtils.insertProfiles(profileRepository)
     }
@@ -72,8 +80,263 @@ class TicketControllerIntegrationTest : AbstractTestcontainersTest() {
                 status().isOk,
                 content().contentType(MediaType.APPLICATION_JSON),
                 jsonPath("$").isArray,
-                jsonPath("$").isNotEmpty
+                jsonPath("$").isNotEmpty,
+                jsonPath("$[*].ticketId").exists(),
+                jsonPath("$[*].title").exists(),
+                jsonPath("$[*].description").exists(),
+                jsonPath("$[*].productId").exists(),
+                jsonPath("$[*].customerId").exists(),
+                jsonPath("$[*].expertId").exists(),
+                jsonPath("$[*].totalExchangedMessages").exists(),
+                jsonPath("$[*].status").exists(),
+                jsonPath("$[*].priorityLevel").exists(),
+                jsonPath("$[*].createdAt").exists(),
+                jsonPath("$[*].lastModifiedAt").exists()
+                )
+    }
+
+    @Test
+    fun getAllTicketsOpen() {
+        mockMvc
+            .perform(get("/API/tickets").queryParam("status", "OPEN").contentType("application/json"))
+            .andExpectAll(
+                status().isOk,
+                content().contentType(MediaType.APPLICATION_JSON),
+                jsonPath("$").isArray,
+                jsonPath("$").isNotEmpty,
+                jsonPath("$[*].ticketId").exists(),
+                jsonPath("$[*].title").exists(),
+                jsonPath("$[*].description").exists(),
+                jsonPath("$[*].productId").exists(),
+                jsonPath("$[*].customerId").exists(),
+                jsonPath("$[*].expertId").exists(),
+                jsonPath("$[*].totalExchangedMessages").exists(),
+                jsonPath("$[*].status").exists(),
+                jsonPath("$[*].priorityLevel").exists(),
+                jsonPath("$[*].createdAt").exists(),
+                jsonPath("$[*].lastModifiedAt").exists(),
+                jsonPath("$[*].status").value(List(2){"OPEN"})
+            // TODO: generalize for all sizes and all status
             )
+    }
+
+    @Test
+    fun getAllTicketsWrongStatus() {
+        mockMvc
+            .perform(get("/API/tickets").queryParam("status", "OPENED").contentType("application/json"))
+            .andExpect(status().isUnprocessableEntity)
+    }
+
+    /////////////////////////////////////////////////////////////////////
+    ////// GET /API/tickets/{ticketId}
+    /////////////////////////////////////////////////////////////////////
+
+    @Test
+    fun getTicketById() {
+        val ticket = testTickets[0]
+        mockMvc
+            .perform(get("/API/tickets/${ticket.id}").contentType("application/json"))
+            .andExpectAll(
+                status().isOk,
+                content().contentType(MediaType.APPLICATION_JSON),
+                jsonPath("$.ticketId").value(ticket.id),
+                jsonPath("$.title").value(ticket.title),
+                jsonPath("$.description").value(ticket.description),
+                jsonPath("$.productId").value(ticket.product.id),
+                jsonPath("$.customerId").value(ticket.customer.id),
+                jsonPath("$.expertId").value(null),
+                jsonPath("$.totalExchangedMessages").value(ticket.messages.size),
+                jsonPath("$.status").value(ticket.status.toString()),
+                jsonPath("$.priorityLevel").value(ticket.priorityLevel),
+                jsonPath("$.createdAt").value(ticket.createdAt),
+                jsonPath("$.lastModifiedAt").value(ticket.lastModifiedAt)
+            )
+    }
+
+    @Test
+    fun getTicketByIdNotFound() {
+        val ticketId = Int.MAX_VALUE
+        mockMvc
+            .perform(get("/API/tickets/${ticketId}").contentType("application/json"))
+            .andExpect(status().isNotFound)
+    }
+
+    @Test
+    fun getTicketByIdWrongValue() {
+        val ticketIdWrongValues = listOf(0, -1, -2, Int.MIN_VALUE)
+
+        ticketIdWrongValues.forEach{
+            mockMvc
+                .perform(get("/API/tickets/${it}").contentType("application/json"))
+                .andExpect(status().isUnprocessableEntity)
+        }
+    }
+
+    @Test
+    fun getTicketByIdWrongType() {
+        mockMvc
+            .perform(get("/API/tickets/wrongType").contentType("application/json"))
+            .andExpect(status().isUnprocessableEntity)
+    }
+
+    /////////////////////////////////////////////////////////////////////
+    ////// GET /API/tickets/{ticketId}/statusChanges
+    /////////////////////////////////////////////////////////////////////
+
+    @Test
+    fun getTicketStatusChangesById() {
+        val ticket = testTickets[0]
+        mockMvc
+            .perform(get("/API/tickets/${ticket.id}/statusChanges").contentType("application/json"))
+            .andExpect(status().isOk)
+        // TODO: to complete
+    }
+
+    /////////////////////////////////////////////////////////////////////
+    ////// POST /API/tickets
+    /////////////////////////////////////////////////////////////////////
+
+    @Test
+    fun createTicket() {
+        val newTicketDTO = TestTicketUtils.getNewTicketDTO()
+
+        val mapper = ObjectMapper()
+        val jsonBody = mapper.writeValueAsString(newTicketDTO)
+
+        mockMvc
+            .perform(post("/API/tickets")
+                .content(jsonBody)
+                .contentType("application/json"))
+            .andExpect(status().isCreated)
+            .andExpect { jsonPath("$.ticketId").exists() }
+    }
+
+    // TODO: all right cases
+
+    @Test
+    fun createTicketCustomerNotFound() {
+        val newTicketDTO = TestTicketUtils.getNewTicketDTO().copy(
+            customerId = Int.MAX_VALUE
+        )
+
+        val mapper = ObjectMapper()
+        val jsonBody = mapper.writeValueAsString(newTicketDTO)
+
+        mockMvc
+            .perform(post("/API/tickets")
+                .content(jsonBody)
+                .contentType("application/json"))
+            .andExpect(status().isNotFound)
+    }
+
+    @Test
+    fun createTicketProductNotFound() {
+        val newTicketDTO = TestTicketUtils.getNewTicketDTO().copy(
+            productId = Int.MAX_VALUE
+        )
+
+        val mapper = ObjectMapper()
+        val jsonBody = mapper.writeValueAsString(newTicketDTO)
+
+        mockMvc
+            .perform(post("/API/tickets")
+                .content(jsonBody)
+                .contentType("application/json"))
+            .andExpect(status().isNotFound)
+    }
+
+    @Test
+    fun createTicketDuplicateCustomerProduct() {
+        val newTicketDTO = testTickets[0].toDTO().copy()
+
+        val mapper = ObjectMapper()
+        val jsonBody = mapper.writeValueAsString(newTicketDTO)
+
+        mockMvc
+            .perform(post("/API/tickets")
+                .content(jsonBody)
+                .contentType("application/json"))
+            .andExpect(status().isConflict)
+    }
+
+    @Test
+    fun createTicketBlankFields() {
+        val newTicketDTO = TestTicketUtils.getNewTicketDTO().copy(
+            title = "",
+            description = ""
+        )
+
+        val mapper = ObjectMapper()
+        val jsonBody = mapper.writeValueAsString(newTicketDTO)
+
+        mockMvc
+            .perform(post("/API/tickets")
+                .content(jsonBody)
+                .contentType("application/json"))
+            .andExpect(status().isUnprocessableEntity)
+            .andExpect { jsonPath("$.error").exists() }
+    }
+
+    @Test
+    fun createTicketFieldsWithSpaces() {
+        val newTicketDTO = TestTicketUtils.getNewTicketDTO().copy(
+            title = "   ",
+            description = " "
+        )
+
+        val mapper = ObjectMapper()
+        val jsonBody = mapper.writeValueAsString(newTicketDTO)
+
+        mockMvc
+            .perform(post("/API/tickets")
+                .content(jsonBody)
+                .contentType("application/json"))
+            .andExpect(status().isUnprocessableEntity)
+            .andExpect { jsonPath("$.error").exists() }
+    }
+
+    @Test
+    fun createTicketInvalidCustomerIdOrProductId() {
+        val newTicketDTO = TestTicketUtils.getNewTicketDTO().copy(
+            customerId = -1,
+            productId = -1
+        )
+
+        val mapper = ObjectMapper()
+        val jsonBody = mapper.writeValueAsString(newTicketDTO)
+
+        mockMvc
+            .perform(post("/API/tickets")
+                .content(jsonBody)
+                .contentType("application/json"))
+            .andExpect(status().isUnprocessableEntity)
+            .andExpect { jsonPath("$.error").exists() }
+    }
+
+    /////////////////////////////////////////////////////////////////////
+    ////// PUT /API/tickets/{ticketId}/start
+    /////////////////////////////////////////////////////////////////////
+
+    @Test
+    fun startTicketById() {
+        val oldTicketDTO = testTickets[0].toDTO()
+        val newTicketDTO = oldTicketDTO.copy(
+            expertId = 1,
+            priorityLevel = "LOW",
+            description = ""
+        )
+
+        val mapper = ObjectMapper()
+        val jsonBody = mapper.writeValueAsString(newTicketDTO)
+
+        println("TUTTI: ${ticketRepository.findAll()}")
+        mockMvc
+            .perform(
+                put("/API/tickets/${oldTicketDTO.ticketId}/start")
+                    .content(jsonBody)
+                    .contentType(MediaType.APPLICATION_JSON)
+            )
+            .andExpect(status().isNoContent)
     }
 
 }
--- server/src/test/kotlin/it/polito/wa2/g29/server/utils/TestTicketUtils.kt
+++ server/src/test/kotlin/it/polito/wa2/g29/server/utils/TestTicketUtils.kt
@@ -1,5 +1,6 @@
 package it.polito.wa2.g29.server.utils
 
+import it.polito.wa2.g29.server.dto.TicketDTO
 import it.polito.wa2.g29.server.model.Product
 import it.polito.wa2.g29.server.model.Profile
 import it.polito.wa2.g29.server.model.Ticket
@@ -11,7 +12,13 @@ object TestTicketUtils {
     lateinit var profiles: List<Profile>
 
     fun insertTickets(ticketRepository: TicketRepository): List<Ticket> {
-        val newTickets = listOf(
+        val newTickets = getTickets()
+        ticketRepository.saveAll(newTickets)
+        return newTickets
+    }
+
+    private fun getTickets(): List<Ticket> {
+        val tickets = listOf(
             Ticket(
                 title = "Broken airpods",
                 description = "My airpods fell and now I can't hear anything",
@@ -25,7 +32,22 @@ object TestTicketUtils {
                 customer = profiles[1]
             )
         )
-        ticketRepository.saveAll(newTickets)
-        return newTickets
+        return tickets
+    }
+
+    fun getNewTicketDTO(): TicketDTO {
+        return TicketDTO(
+            ticketId = null,
+            title = "newtitle",
+            description = "newdescription",
+            productId = 1,
+            customerId = 2,
+            expertId = null,
+            totalExchangedMessages = 0,
+            status = "OPEN",
+            priorityLevel = null,
+            createdAt = 0,
+            lastModifiedAt = 0
+        )
     }
 }
