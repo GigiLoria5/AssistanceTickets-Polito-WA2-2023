855841a7b62feada7845917e0a346dd78ee731fe
Andred00 <s303339@studenti.polito.it>
2023-05-16 11:54:10 +0200

    feat: authorization tests for products

--- server/src/test/kotlin/it/polito/wa2/g29/server/integration/controller/ProductControllerIT.kt
+++ server/src/test/kotlin/it/polito/wa2/g29/server/integration/controller/ProductControllerIT.kt
@@ -1,9 +1,16 @@
 package it.polito.wa2.g29.server.integration.controller
 
 import it.polito.wa2.g29.server.integration.AbstractTestcontainersTest
+import it.polito.wa2.g29.server.model.Expert
 import it.polito.wa2.g29.server.model.Product
+import it.polito.wa2.g29.server.model.Profile
+import it.polito.wa2.g29.server.repository.ExpertRepository
 import it.polito.wa2.g29.server.repository.ProductRepository
+import it.polito.wa2.g29.server.repository.ProfileRepository
+import it.polito.wa2.g29.server.utils.ExpertTestUtils
 import it.polito.wa2.g29.server.utils.ProductTestUtils
+import it.polito.wa2.g29.server.utils.ProfileTestUtils
+import it.polito.wa2.g29.server.utils.SecurityTestUtils
 import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
@@ -20,20 +27,32 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
 import org.springframework.transaction.annotation.Transactional
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@AutoConfigureMockMvc(addFilters = false)
+@AutoConfigureMockMvc
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class ProductControllerIT : AbstractTestcontainersTest() {
     @Autowired
     private lateinit var mockMvc: MockMvc
 
     @Autowired
+    private lateinit var profileRepository: ProfileRepository
+
+    @Autowired
+    private lateinit var expertRepository: ExpertRepository
+
+    @Autowired
     private lateinit var productRepository: ProductRepository
 
     lateinit var testProducts: List<Product>
 
+    lateinit var testProfiles: List<Profile>
+
+    lateinit var testExperts: List<Expert>
+
     @BeforeAll
     fun setup() {
         testProducts = ProductTestUtils.insertProducts(productRepository)
+        testProfiles = ProfileTestUtils.insertProfiles(profileRepository)
+        testExperts = ExpertTestUtils.insertExperts(expertRepository)
     }
 
     /////////////////////////////////////////////////////////////////////
@@ -44,6 +63,7 @@ class ProductControllerIT : AbstractTestcontainersTest() {
     @Transactional
     @Rollback
     fun getAllProductsEmpty() {
+        SecurityTestUtils.setClient(testProfiles[0].email)
         productRepository.deleteAllInBatch()
         mockMvc
             .perform(get("/API/products").contentType("application/json"))
@@ -56,7 +76,50 @@ class ProductControllerIT : AbstractTestcontainersTest() {
     }
 
     @Test
-    fun getAllProducts() {
+    fun getAllProductsClient() {
+        SecurityTestUtils.setClient(testProfiles[0].email)
+        mockMvc
+            .perform(get("/API/products").contentType("application/json"))
+            .andExpectAll(
+                status().isOk,
+                content().contentType(MediaType.APPLICATION_JSON),
+                jsonPath("$").isArray,
+                jsonPath("$").isNotEmpty,
+                jsonPath("$[*].productId").exists(),
+                jsonPath("$[*].asin").exists(),
+                jsonPath("$[*].brand").exists(),
+                jsonPath("$[*].category").exists(),
+                jsonPath("$[*].manufacturerNumber").exists(),
+                jsonPath("$[*].name").exists(),
+                jsonPath("$[*].price").exists(),
+                jsonPath("$[*].weight").exists()
+            )
+    }
+
+    @Test
+    fun getAllProductsExpert() {
+        SecurityTestUtils.setExpert(testExperts[0].email)
+        mockMvc
+            .perform(get("/API/products").contentType("application/json"))
+            .andExpectAll(
+                status().isOk,
+                content().contentType(MediaType.APPLICATION_JSON),
+                jsonPath("$").isArray,
+                jsonPath("$").isNotEmpty,
+                jsonPath("$[*].productId").exists(),
+                jsonPath("$[*].asin").exists(),
+                jsonPath("$[*].brand").exists(),
+                jsonPath("$[*].category").exists(),
+                jsonPath("$[*].manufacturerNumber").exists(),
+                jsonPath("$[*].name").exists(),
+                jsonPath("$[*].price").exists(),
+                jsonPath("$[*].weight").exists()
+            )
+    }
+
+    @Test
+    fun getAllProductsManager() {
+        SecurityTestUtils.setManager()
         mockMvc
             .perform(get("/API/products").contentType("application/json"))
             .andExpectAll(
@@ -81,6 +144,7 @@ class ProductControllerIT : AbstractTestcontainersTest() {
 
     @Test
     fun getProductById() {
+        SecurityTestUtils.setClient(testProfiles[0].email)
         val product = testProducts[0]
         mockMvc
             .perform(get("/API/products/${product.id}").contentType("application/json"))
@@ -100,6 +164,7 @@ class ProductControllerIT : AbstractTestcontainersTest() {
 
     @Test
     fun getProductByIdNotFound() {
+        SecurityTestUtils.setClient(testProfiles[0].email)
         mockMvc
             .perform(get("/API/products/99999999").contentType("application/json"))
             .andExpect(status().isNotFound)
@@ -110,6 +175,7 @@ class ProductControllerIT : AbstractTestcontainersTest() {
         ints = [0, -1, -2, Int.MIN_VALUE]
     )
     fun getProductByInvalidId(id: Int) {
+        SecurityTestUtils.setClient(testProfiles[0].email)
         mockMvc
             .perform(get("/API/products/${id}").contentType("application/json"))
             .andExpect(status().isUnprocessableEntity)
@@ -117,6 +183,7 @@ class ProductControllerIT : AbstractTestcontainersTest() {
 
     @Test
     fun getProductByIdWrongType() {
+        SecurityTestUtils.setClient(testProfiles[0].email)
         mockMvc
             .perform(get("/API/products/wrongTypeId").contentType("application/json"))
             .andExpect(status().isUnprocessableEntity)
