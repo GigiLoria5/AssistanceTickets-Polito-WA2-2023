189905282b7f71a8839839b1565f960be59563fe
Luigi Loria <41837640+GigiLoria5@users.noreply.github.com>
2023-06-28 18:09:49 +0200

    feat: add routes protection

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,15 @@
+import {useState} from "react";
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Products from "./components/Products";
 import Profiles from "./components/Profiles";
 import Navbar from "./components/Navbar";
 import NotFoundPage from "./components/NotFoundPage";
-import {useState} from "react";
-import {ROLE_CLIENT, ROLE_EXPERT, ROLE_MANAGER} from "./utils/constants";
 import ManagerDashboard from "./components/ManagerDashboard";
 import ExpertDashboard from "./components/ExpertDashboard";
 import ClientDashboard from "./components/ClientDashboard";
+import {UserRole} from "../enums/UserRole";
+import LoginForm from "./components/LoginForm";
+import ProtectedRoute from "./utils/ProtectedRoute";
 
 function App() {
     return (
@@ -18,18 +20,24 @@ function App() {
 }
 
 function Root() {
-    const [loggedIn, setLoggedIn] = useState(false);
-    const [userInfo, setUserInfo] = useState(false);
+    const [userInfo, setUserInfo] = useState(null);
 
-    // TODO: once we have login remove hardcoded role
     return (
         <Routes>
-            <Route path="/" element={<Navbar/>}>
-                <Route index element={renderDashboard(ROLE_MANAGER)}/>
-                <Route path='/products' element={<Products/>}/>
-                <Route path='/profiles' element={<Profiles/>}/>
+            <Route path="/" element={
+                <ProtectedRoute
+                    userInfo={userInfo}
+                    setUserInfo={setUserInfo}
+                    rolesAllowed={[UserRole.MANAGER, UserRole.EXPERT, UserRole.CLIENT]}
+                />}>
+                <Route path="" element={<Navbar userInfo={userInfo}/>}>
+                    <Route index element={renderDashboard(userInfo ? userInfo.role : "")}/>
+                    <Route path='/products' element={<Products/>}/>
+                    <Route path='/profiles' element={<Profiles/>}/>
+                </Route>
             </Route>
 
+            <Route path='/login' element={<LoginForm/>}/>
             <Route path='*' element={<NotFoundPage/>}/>
         </Routes>
     );
@@ -37,11 +45,11 @@ function Root() {
 
 function renderDashboard(userRole) {
     switch (userRole) {
-        case ROLE_MANAGER:
+        case UserRole.MANAGER:
             return <ManagerDashboard/>;
-        case ROLE_EXPERT:
+        case UserRole.EXPERT:
             return <ExpertDashboard/>;
-        case ROLE_CLIENT:
+        case UserRole.CLIENT:
             return <ClientDashboard/>;
         default:
             return <NotFoundPage/>;
