c4b804c4e2ddb984019a0a0ed609e5a6242ea7a4
antoniocolelli <colelli16@gmail.com>
2023-07-07 09:35:42 +0200

    feat: validation of register product token size

--- server/src/main/kotlin/it/polito/wa2/g29/server/dto/RegisterTokenDTO.kt
+++ server/src/main/kotlin/it/polito/wa2/g29/server/dto/RegisterTokenDTO.kt
@@ -1,7 +1,8 @@
 package it.polito.wa2.g29.server.dto
 
-import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.Size
 
 data class RegisterTokenDTO(
-    @field:NotBlank
-    val token: String)
+    @field:Size(min = 36, max = 36)
+    val token: String
+)
