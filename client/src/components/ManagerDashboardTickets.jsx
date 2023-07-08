f27d3ee9454aaafbc2c540983e886ec5284ab9e2
Andred00 <s303339@studenti.polito.it>
2023-07-08 18:30:21 +0200

    feat: fill manager dashboard with tickets

--- client/src/components/ManagerDashboardTickets.jsx
+++ client/src/components/ManagerDashboardTickets.jsx
@@ -1,44 +1,159 @@
 import { useEffect, useState } from "react";
-import {useStatusAlert} from "../../hooks/useStatusAlert";
+import { useStatusAlert } from "../../hooks/useStatusAlert";
 import API from "../API";
-import {Button, Col, Container, Form, Modal, Row, Spinner, Table} from "react-bootstrap";
+import { Button, Col, Container, Form, Modal, Row, Spinner, Table, Offcanvas } from "react-bootstrap";
 import { handleApiError } from "../utils/utils";
 import Tickets from "./Tickets";
+import { useNavigate } from "react-router-dom";
 
 function ManagerDashboardTickets() {
     const [tickets, setTickets] = useState(null);
+    const [products, setProducts] = useState(null);
+    const [clientInfo, setClientInfo] = useState(null)
+    const [expertInfo, setExpertInfo] = useState(null)
+    const [showInfoToManager, setShowInfoToManager] = useState(true);
     const [refreshTickets, setRefreshTickets] = useState(true)
+    const [loading, setLoading] = useState(true)
+    const [load, setLoad] = useState(true)
+    const [errorPresence, setErrorPresence] = useState(false)
     const { StatusAlertComponent, showError, resetStatusAlert } = useStatusAlert();
 
     useEffect(() => {
-        if (refreshTickets)
-            API.getAllTickets()
-                .then((response) => {
-                    setTickets(response)
-                    resetStatusAlert()
-                })
-                .catch(err => handleApiError(err, showError))
-                .finally(_ => {
-                    setRefreshTickets(false)
-                })
-    }, [refreshTickets])
+        const getData = async () => {
+            await Promise.all([getTicketsData(), getProductsData()])
+        }
+        if (load === true) {
+            setLoading(true)
+            getData().then(() => {
+                setLoading(false)
+                resetStatusAlert()
+            }).catch(err => stopAnimationAndShowError(err)).finally(() => {
+                setLoad(false)
+            })
+        }
+    }
+        ,
+        [load]
+    )
+
+    const getTicketsData = () => {
+        API.getAllTickets()
+            .then((t) => {
+                setTickets(t)
+                resetStatusAlert()
+            }
+            ).catch(err => handleApiError(err, showError))
+    }
+
+    const getProductsData = () => {
+        API.getAllProducts()
+            .then((p) => {
+                setProducts(p)
+                resetStatusAlert()
+            }
+            ).catch(err => handleApiError(err, showError))
+    }
+
+    const getClientInfo = (clientId) => {
+        API.getProfileById(clientId)
+            .then((client) => {
+                setClientInfo(client)
+                resetStatusAlert
+            })
+            .catch(err => handleApiError(err, showError))
+    }
+
+    const getExpertInfo = (expertId) => {
+        API.getExpertById(expertId)
+            .then((expert) => {
+                setExpertInfo(expert)
+                resetStatusAlert
+            })
+            .catch(err => handleApiError(err, showError))
+    }
+
+    const navigate = useNavigate();
+
+    const actionGoToTicket = (ticket) => {
+        navigate(`/tickets/${ticket.ticketId}`)
+    }
+
+    const formatTickets = () => {
+        return tickets.map(ticket => {
+            const product = products ? products.find(p => p.productId === ticket.productId) : ""
+            return { ...ticket, "product": product.name }
+        })
+    }
+
+    const [showClient, setShowClient] = useState(false)
+    const [showExpert, setShowExpert] = useState(false)
+
+    const handleCloseClient = () => setShowClient(false)
+    const handleCloseExpert = () => setShowExpert(false)
+
+    const handleShowClientInfo =  (clientId) => {
+        getClientInfo(clientId);
+        setShowClient(true)
+    }
+
+    const handleShowExpertInfo =  (expertId) => {
+        getExpertInfo(expertId);
+        setShowExpert(true)
+    }
 
     return (
         <Container className="h-100">
             <Row className="h-100">
                 <Col className="d-flex flex-column align-items-center justify-content-center">
-                    <h2>Tickets Management</h2><StatusAlertComponent/>
+                    <h2>Tickets Management</h2><StatusAlertComponent />
                     {
                         tickets || !refreshTickets ?
                             <>
-                                <Tickets tickets={tickets}/>
-                            </>
-                            : <Spinner animation="border" variant="primary"/>
+                                <Tickets tickets={formatTickets()}
+                                    actionName={"Details"}
+                                    action={actionGoToTicket}
+                                    showInfoToManager={showInfoToManager}
+                                    showClientInfo={handleShowClientInfo}
+                                    showExpertInfo={handleShowExpertInfo}
+                                    />
+                                    {clientInfo ? 
+                                    <ClientInfo show={showClient} handleClose={handleCloseClient} info={clientInfo} title={"Customer Info"}/> : null}
+                                    {expertInfo ? 
+                                    <ExpertInfo show={showExpert} handleClose={handleCloseExpert} info={expertInfo} title={"Expert Info"}/> : null}
+                                    </>
+                            : <Spinner animation="border" variant="primary" />
                     }
                 </Col>
             </Row>
         </Container>
     );
+}
+
+function ClientInfo({show, handleClose, info, title}) {
+    return <Offcanvas show={show} onHide={handleClose}>
+            <Offcanvas.Header closeButton>
+            <Offcanvas.Title >{title}</Offcanvas.Title>
+            </Offcanvas.Header>
+            <Offcanvas.Body>
+                {<h4>Name:  {info.name}</h4> }
+                {<h4>Surname:  {info.surname}</h4> }
+                {<h4>Phone:  {info.phoneNumber}</h4> }
+                {<h4>E-mail:  {info.email}</h4> }
+            </Offcanvas.Body>
+        </Offcanvas>
+}
+
+function ExpertInfo({show, handleClose, info, title}) {
+    return <Offcanvas show={show} onHide={handleClose}>
+            <Offcanvas.Header closeButton>
+            <Offcanvas.Title >{title}</Offcanvas.Title>
+            </Offcanvas.Header>
+            <Offcanvas.Body>
+                {<h4>Name:  {info.name}</h4> }
+                {<h4>Surname:  {info.surname}</h4> }
+                {<h4>E-mail:  {info.email}</h4> }
+            </Offcanvas.Body>
+        </Offcanvas>
 }
 
 export default ManagerDashboardTickets;
--- client/src/components/Tickets.jsx
+++ client/src/components/Tickets.jsx
@@ -1,8 +1,8 @@
-import {Button, Col, Row, Table} from "react-bootstrap";
+import { Button, Col, Row, Table } from "react-bootstrap";
 import React from "react";
-import {dateTimeMillisFormatted} from "../utils/utils";
+import { dateTimeMillisFormatted } from "../utils/utils";
 
-function Tickets({tickets, title, actionName, action, showClientInfo}) {
+function Tickets({ tickets, title, actionName, action, showClientInfo, showExpertInfo, showInfoToManager }) {
 
     /* Here it is not possible to load the tickets list here, because this component can be used,
     * for example, both from client (which sees only his ticket) and manager (which sees all tickets)*/
@@ -15,7 +15,7 @@ function Tickets({tickets, title, actionName, action, showClientInfo}) {
             <Row>
                 <Col>
                     <TicketsTable tickets={tickets} actionName={actionName}
-                                  action={action} showClientInfo={showClientInfo}/>
+                        action={action} showClientInfo={showClientInfo} showExpertInfo={showExpertInfo} showInfoToManager={showInfoToManager} />
                 </Col>
             </Row>
         </>
@@ -23,60 +23,56 @@ function Tickets({tickets, title, actionName, action, showClientInfo}) {
     )
 }
 
-function TicketsTable({tickets, actionName, action, showClientInfo}) {
+function TicketsTable({ tickets, actionName, action, showClientInfo, showExpertInfo, showInfoToManager }) {
     return (
         (tickets.length) > 0 ?
             <div className="table-responsive">
                 <Table>
                     <thead>
-                    <tr>
-                        {tickets[0].product !== undefined ?
-                            <th>Product</th>
-                            : <th>Product id</th>
-                        }
-                        <th>Title</th>
-                        <th>Description</th>
-                        {showClientInfo ? <th>Customer info</th> : null}
-                        {/*
-                        <th>Expert id</th>
-                        <th>Total Exchanged Messages</th>
-                        */}
-                        <th>Status</th>
-                        <th>Priority Level</th>
-                        <th>Created At</th>
-                        <th>Last Modified At</th>
-                        <th></th>
+                        <tr>
+                            {tickets[0].product !== undefined ?
+                                <th>Product</th>
+                                : <th>Product id</th>
+                            }
+                            <th>Title</th>
+                            <th>Description</th>
+                            {showClientInfo ? <th>Customer info</th> : null}
+                            {showExpertInfo ? <th>Expert info</th> : null}
+                            {showInfoToManager ? <th>Total Exchanged Messages</th> : null}
+                            <th>Status</th>
+                            <th>Priority Level</th>
+                            <th>Created At</th>
+                            <th>Last Modified At</th>
+                            <th></th>
 
-                    </tr>
+                        </tr>
                     </thead>
                     <tbody>
-                    {tickets.map((ticket) => (
-                        <tr key={ticket.ticketId}>
-                            {ticket.product !== undefined ?
-                                <td>{ticket.product}</td>
-                                : <td>{ticket.productId}</td>
-                            }
-                            <td>{ticket.title}</td>
-                            <td>{ticket.description}</td>
-                            {showClientInfo ? <td> <Button onClick={()=>showClientInfo(ticket.customerId)}>Show</Button> </td> : null}
-                            {/*
-                            <td>{ticket.expertId}</td>
-                            <td>{ticket.totalExchangedMessages}</td>
-                            */}
-                            <td>{ticket.status}</td>
-                            <td>{ticket.priorityLevel}</td>
-                            <td>{dateTimeMillisFormatted(ticket.createdAt)}</td>
-                            <td>{dateTimeMillisFormatted(ticket.lastModifiedAt)}</td>
-                            {action !== undefined ?
-                                <td>
-                                    <Button onClick={() => action(ticket)}>
-                                        {actionName}
-                                    </Button>
-                                </td>
-                                : null
-                            }
-                        </tr>
-                    ))}
+                        {tickets.map((ticket) => (
+                            <tr key={ticket.ticketId}>
+                                {ticket.product !== undefined ?
+                                    <td>{ticket.product}</td>
+                                    : <td>{ticket.productId}</td>
+                                }
+                                <td>{ticket.title}</td>
+                                <td>{ticket.description}</td>
+                                {showClientInfo ? <td> <Button onClick={() => showClientInfo(ticket.customerId)}>Show</Button> </td> : null}
+                                {showExpertInfo ? <td> <Button onClick={() => showExpertInfo(ticket.expertId)}>Show</Button> </td> : null}
+                                {showInfoToManager ? <td>{ticket.totalExchangedMessages}</td> : null}
+                                <td>{ticket.status}</td>
+                                <td>{ticket.priorityLevel}</td>
+                                <td>{dateTimeMillisFormatted(ticket.createdAt)}</td>
+                                <td>{dateTimeMillisFormatted(ticket.lastModifiedAt)}</td>
+                                {action !== undefined ?
+                                    <td>
+                                        <Button onClick={() => action(ticket)}>
+                                            {actionName}
+                                        </Button>
+                                    </td>
+                                    : null
+                                }
+                            </tr>
+                        ))}
                     </tbody>
                 </Table>
             </div> : <div>No tickets found</div>
